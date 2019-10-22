be47baa98f757e856a087ffd1f38e20bc8e6a303
Daniel Elliott <elliott.daniel.ballantyne@gmail.com>
2019-10-22 12:05:44 -0600

    added editing functionality, database now autoincrements id

--- src/Main.java
+++ src/Main.java
@@ -8,11 +8,11 @@ import java.util.List;
 import java.util.ArrayList;
 
 public class Main extends JFrame {
+    public final String[] brands = new String[]{"Trek", "Santa Cruz", "YT", "Commencal"};
     private JTabbedPane mainPane = new JTabbedPane();
     private JPanel viewPanel = new JPanel();
     private JPanel addPanel = new JPanel();
-    private JTextField idField = new JTextField(2);
-    private JComboBox brandBox = new JComboBox(new String[]{"Trek", "Santa Cruz", "YT", "Commencal"});
+    private JComboBox brandBox = new JComboBox(brands);
     private JTextField modelField = new JTextField(20);
     private JTextField priceField = new JTextField(8);
     private JButton confirmButton = new JButton("Confirm");
@@ -27,11 +27,13 @@ public class Main extends JFrame {
         confirmButton.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent ev) {
                 try {
-                    submit(Integer.parseInt(idField.getText()), brandBox.getSelectedItem().toString(), modelField.getText(), Integer.parseInt(priceField.getText()));
+                    submit(brandBox.getSelectedItem().toString(), modelField.getText(), Integer.parseInt(priceField.getText()));
+                    JOptionPane.showMessageDialog(null, "Product successfully added to inventory.");
                 }
                 catch (Exception ex) {
                     JOptionPane.showMessageDialog(null, "Please make sure all fields have a value before proceeding.");
                 }
+                viewModel.setDataVector(getTableContents(), new String[]{"ID", "Brand", "Model", "MSRP"}); //update table locally
             }
         });
         viewTable.addMouseListener(new MouseAdapter() {
@@ -55,9 +57,8 @@ public class Main extends JFrame {
         viewTable.setMinimumSize(new Dimension(400,400));
         viewTable.setBorder(BorderFactory.createLineBorder(java.awt.Color.black));
         viewPanel.add(new JScrollPane(viewTable));
-        mainPane.add(viewPanel, "View Inventory");
+        mainPane.add(viewPanel, "View & Edit Inventory");
         //addpanel
-        addPanel.add(idField);
         addPanel.add(brandBox);
         addPanel.add(modelField);
         addPanel.add(priceField);
@@ -86,13 +87,27 @@ public class Main extends JFrame {
         }
         return contents;
     }
-    public void submit(int id, String brand, String model, int price) {
+    public void submit(String brand, String model, int price) {
         try
         {
             Class.forName("com.mysql.cj.jdbc.Driver");
             Connection conn = null;
             conn = DriverManager.getConnection("jdbc:mysql://localhost/dbtest","root", "");
-            conn.createStatement().execute("INSERT INTO bikes " + "VALUES (" + id + ", '" + brand + "', '" + model + "', " + price + ")");
+            conn.createStatement().execute("INSERT INTO bikes (brand, model, price) " + "VALUES ('" + brand + "', '" + model + "', " + price + ")");
+            System.out.println("statement executed");
+            conn.close();
+        }
+        catch(Exception e) {
+            System.out.println(e.getMessage());
+        }
+    }
+    public void update(int id, String brand, String model, int price) {
+        try
+        {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            Connection conn = null;
+            conn = DriverManager.getConnection("jdbc:mysql://localhost/dbtest","root", "");
+            conn.createStatement().execute("UPDATE bikes " + "SET brand = '" + brand + "', model = '" + model + "', price = " + price + " WHERE id = " + id);
             System.out.println("statement executed");
             conn.close();
         }
@@ -101,7 +116,37 @@ public class Main extends JFrame {
         }
     }
     public void edit(int row) {
-        System.out.println(row);
+        JFrame editWindow = new JFrame("Edit Product");
+        editWindow.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+        editWindow.setPreferredSize(new Dimension(500, 110));
+        editWindow.setMinimumSize(new Dimension(500, 110));
+        editWindow.setResizable(false);
+        editWindow.setLocationRelativeTo(null);
+        JPanel editPanel = new JPanel();
+        JComboBox brandBox = new JComboBox(brands);
+        brandBox.setSelectedItem(viewModel.getValueAt(row, 1));
+        JTextField modelField = new JTextField(viewModel.getValueAt(row, 2).toString(), 20);
+        JTextField priceField = new JTextField(viewModel.getValueAt(row, 3).toString(), 8);
+        JButton confirmButton = new JButton("Confirm");
+        confirmButton.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent ev) {
+                try {
+                    update(Integer.parseInt(viewModel.getValueAt(row, 0).toString()), brandBox.getSelectedItem().toString(), modelField.getText(), Integer.parseInt(priceField.getText()));
+                }
+                catch (Exception ex) {
+                    JOptionPane.showMessageDialog(null, "Please make sure all fields have a value before proceeding.");
+                    System.out.println(ex.getMessage());
+                }
+                viewModel.setDataVector(getTableContents(), new String[]{"ID", "Brand", "Model", "MSRP"}); //update table locally
+                editWindow.dispose();
+            }
+        });
+        editPanel.add(brandBox);
+        editPanel.add(modelField);
+        editPanel.add(priceField);
+        editPanel.add(confirmButton);
+        editWindow.add(editPanel);
+        editWindow.setVisible(true);
     }
     public static void main(String[] args) {
         EventQueue.invokeLater(new Runnable() {
